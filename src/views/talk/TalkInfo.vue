a88a1582cbff27c33f5999cddc00649aad554bf6
gmy <1508594767@qq.com>
2022-06-27 16:01:00 +0800

    说说列表

---
+++ src/views/talk/TalkInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- banner -->
+    <div class="banner" :style="cover">
+      <h1 class="banner-title">说说</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TalkInfo.vue",
+  data(){
+    return {
+
+    }
+  },
+  computed:{
+    cover() {
+      let cover = "";
+      this.$store.state.blogInfo.backgroundList.forEach(item => {
+        if (item.backgroundLabel === "talk") {
+          cover = item.backgroundLabel;
+        }
+      });
+      return "background: url(" + cover + ") center center / cover no-repeat";
+    },
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
